5aa040df76e72cf91353ae4bd6357aa9d236f601
Justin Kline <justakline@gmail.com>
2019-05-15 11:07:38 -0400

    test commit

--- UI/src/Test.java
+++ UI/src/Test.java
@@ -2,5 +2,8 @@ public class Test {
     //hi this is a comment
     public Test(){
         System.out.println("alll the advisories");
+        for (int i = 0; i < 10; i ++){
+            System.out.println(i);
+        }
     }
 }
