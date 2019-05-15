90423aebdcfec47607b10b9dd944624db171c232
Ryan Leventhal <ryan.levent@gmail.com>
2019-05-15 12:47:28 -0400

    test2

--- UI/src/Test.java
+++ UI/src/Test.java
@@ -1,2 +1,20 @@
-public class Test {
+import javax.swing.*;
+
+public class Test extends JFrame {
+
+    private JPanel root = new JPanel();
+    private JButton button = new JButton();
+    private JButton button1 = new JButton();
+
+    public Test() {
+
+        add(root);
+        setTitle("Hello There!");
+        setSize(400, 400);
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.isVisible();
+    }
 }
