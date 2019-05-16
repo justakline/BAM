adec1c233736da0220e0a483e10ccf0468007e73
Justin Kline <justakline@gmail.com>
2019-05-16 11:40:40 -0400

    Created a Toolbar with dropdown features... need to add mroe but a start

--- UI/src/GUI.java
+++ UI/src/GUI.java
@@ -1,4 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+import static java.awt.FlowLayout.LEFT;
 
 public class GUI extends JFrame {
 
@@ -9,27 +13,42 @@ public class GUI extends JFrame {
         setTitle("Hello There!");
         setSize(400, 400);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+        JPanel panel=new JPanel();
+
+
+        ToolBar toolBar= new ToolBar();
+
+
+
+
+        this.setSize(400,400);
+        this.setLayout(null);
+        this.setVisible(true);
+    setJMenuBar(toolBar);
+
+
+
+
+//
+//        ToolBar toolBar = new ToolBar();
+//        JPanel panel = new JPanel(new FlowLayout());
+//        panel.setBounds(40,80,10,10);
+////        panel.setBackground(Color.BLUE);
+//
+//        panel.add(toolBar);
+//        this.add(panel);
+
+
+
+
+
     }
 
     public static void main(String[] args) {
         GUI test = new GUI();
         test.setVisible(true);
-        JInternalFrame frame2 = new JInternalFrame();
-
-        frame2.setTitle("here I stand");
-
-        JButton button1 = new JButton("a button");
-
-
-        ToolBar toolBar = new ToolBar();
-        JPanel panel = new JPanel();
-        panel.add(button1);
-
-        frame2.setVisible(true);
-        frame2.add(panel);
-        root.add(frame2);
-        root.setVisible(true);
-
 
 
 
--- UI/src/ToolBar.java
+++ UI/src/ToolBar.java
@@ -1,15 +1,51 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
 public class ToolBar extends JMenuBar {
-    public JMenu menu;
+
+
+
+
 
     public ToolBar(){
-        menu = new JMenu("File");
-        menu.setMnemonic(KeyEvent.VK_A);
-        menu.getAccessibleContext().setAccessibleDescription(
-                "The only menu in this program that has menu items");
+        super();
 
-        this.add(menu);
+        JMenu file = new JMenu("File");
+
+        file.setMnemonic(KeyEvent.VK_A);
+        file.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        JMenuItem newThing = new JMenuItem("New");
+        file.add(newThing);
+        this.add(file);
+
+
+
+
+        JMenu load = new JMenu("Load");
+
+        file.setMnemonic(KeyEvent.VK_A);
+        file.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        this.add(load);
+
+
+        JMenu run = new JMenu("Run");
+
+        file.setMnemonic(KeyEvent.VK_A);
+        file.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        this.add(run);
+
+        JMenu help = new JMenu("Help");
+
+        file.setMnemonic(KeyEvent.VK_A);
+        file.getAccessibleContext().setAccessibleDescription(
+                "The only menu in this program that has menu items");
+        this.add(help);
+
     }
+
+
 }
