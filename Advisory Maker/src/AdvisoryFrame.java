314f5b00117c4c425e0169b146230a68268f8596
Justin Kline <justakline@gmail.com>
2019-05-22 12:49:04 -0400

    Created a Drag and Drop System, but when dropping, it just overwrites the data

--- Advisory Maker/src/AdvisoryFrame.java
+++ Advisory Maker/src/AdvisoryFrame.java
@@ -3,6 +3,12 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.io.IOException;
 import java.util.Vector;
 
 public class AdvisoryFrame extends JInternalFrame implements ListSelectionListener {
@@ -52,6 +58,66 @@ public class AdvisoryFrame extends JInternalFrame implements ListSelectionListen
 
 
         table= new JTable( tableData, columnNames);
+
+        table.setTransferHandler(new TransferHandler(){
+
+            public int getSourceActions(JComponent c) {
+                return DnDConstants.ACTION_COPY_OR_MOVE;
+            }
+
+            public Transferable createTransferable(JComponent comp)
+            {
+                JTable table=(JTable)comp;
+                int row=table.getSelectedRow();
+                int col=table.getSelectedColumn();
+
+                String value = (String)table.getModel().getValueAt(row,col);
+                StringSelection transferable = new StringSelection(value);
+                table.getModel().setValueAt(null,row,col);
+                return transferable;
+            }
+            public boolean canImport(TransferHandler.TransferSupport info){
+                if (!info.isDataFlavorSupported(DataFlavor.stringFlavor)){
+                    return false;
+                }
+
+                return true;
+            }
+
+            public boolean importData(TransferSupport support) {
+
+                if (!support.isDrop()) {
+                    return false;
+                }
+
+                if (!canImport(support)) {
+                    return false;
+                }
+
+                JTable table=(JTable)support.getComponent();
+                DefaultTableModel tableModel=(DefaultTableModel)table.getModel();
+
+                JTable.DropLocation dl = (JTable.DropLocation)support.getDropLocation();
+
+                int row = dl.getRow();
+                int col=dl.getColumn();
+
+                String data;
+                try {
+                    data = (String)support.getTransferable().getTransferData(DataFlavor.stringFlavor);
+                } catch (UnsupportedFlavorException e) {
+                    return false;
+                } catch (IOException e) {
+                    return false;
+                }
+
+                tableModel.setValueAt(data, row, col);
+
+                return true;
+            }
+
+        });
+        
         JScrollPane p = new JScrollPane(table);
         p.setPreferredSize(new Dimension(50,100));
         add(p);
