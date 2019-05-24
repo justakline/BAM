8ad69345ffbdc8aabd145721a724d20f177c5e98
Ryan Leventhal <ryan.levent@gmail.com>
2019-05-24 09:19:58 -0400

    Added dialog options to load file (doesn't actually load files yet).

---
+++ Advisory Maker/src/Algorithm.java
@@ -0,0 +1,8 @@
+public class Algorithm {
+
+    private Algorithm() {
+
+    }
+
+
+}
--- Advisory Maker/src/ToolBar.java
+++ Advisory Maker/src/ToolBar.java
@@ -1,25 +1,59 @@
 import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ToolBar extends JMenuBar {
+public class ToolBar extends JMenuBar implements ActionListener, MenuListener {
+
+    private final JFileChooser fc;
+    private JMenu file;
+    private JMenu load;
+    private JMenu run;
+    private JMenu help;
+    private JMenu options;
 
     public ToolBar(){
         super();
+        fc = new JFileChooser();
+        JMenuItem temp;
 
         //Creation of the file Tab and associated submenu items
-        JMenu file = new JMenu("File");
-        file.add(new JMenuItem("New"));
+        this.add(file = new JMenu("File"));
+        file.add(temp = new JMenuItem("New"));
         file.add(new JMenuItem("Open"));
         file.add(new JMenuItem("Print"));
-        this.add(file);
 
-        this.add(new JMenu("Load"));
-        this.add(new JMenu("Run"));
-        this.add(new JMenu("Help"));
+        this.add(load = new JMenu("Load"));
+        load.add(temp = new JMenuItem("Load Student List"));
+        temp.addActionListener(this);
+        load.add(temp = new JMenuItem("Load Activities List"));
+        temp.addActionListener(this);
 
-        JMenu options = new JMenu("Options");
+        this.add(run = new JMenu("Run"));
+        this.add(help = new JMenu("Help"));
+
+        this.add(options = new JMenu("Options"));
         options.add(new JMenuItem("Set to One Window"));
-        this.add(options);
     }
 
+    @Override
+    public void menuSelected(MenuEvent e) {
 
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        fc.showOpenDialog(GUI.RightPane.getRightPane());
+    }
 }
